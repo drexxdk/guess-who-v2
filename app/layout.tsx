6d06906a41e0fa0056254985204346db5080bdd7
Frederik <fni@lruddannelse.dk>
2026-02-06 08:05:42 +0100

    tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.className} antialiased`}>
         <ServiceWorkerRegistration />
         <AuthStateListener />
